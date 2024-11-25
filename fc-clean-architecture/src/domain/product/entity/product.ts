b259c19d2439334bc0d596e97ae8e0c92c96e7db
AngeloSouza1 <angeloafdesouza@gmail.com>
2024-11-25 10:43:40 -0300

    feat: validation process[product]

--- fc-clean-architecture/src/domain/product/entity/product.ts
+++ fc-clean-architecture/src/domain/product/entity/product.ts
@@ -1,12 +1,13 @@
 import Notification from "../../@shared/notification/notification";
 import NotificationError from "../../@shared/notification/notification.error";
 import ProductInterface from "./product.interface";
+import ProductValidator from "../validator/product.validator";
 
 export default class Product implements ProductInterface {
   private _id: string;
   private _name: string;
   private _price: number;
-  private notification: Notification;
+  public notification: Notification;
 
   constructor(id: string, name: string, price: number) {
     this._id = id;
@@ -35,33 +36,23 @@ export default class Product implements ProductInterface {
   changeName(name: string): void {
     this._name = name;
     this.validate();
+
+    if (this.notification.hasErrors()) {
+      throw new NotificationError(this.notification.getErrors());
+    }
   }
 
   changePrice(price: number): void {
     this._price = price;
     this.validate();
+
+    if (this.notification.hasErrors()) {
+      throw new NotificationError(this.notification.getErrors());
+    }
   }
 
   private validate(): void {
-    if (!this._id) {
-      this.notification.addError({
-        context: "product",
-        message: "Id is required",
-      });
-    }
-
-    if (!this._name) {
-      this.notification.addError({
-        context: "product",
-        message: "Name is required",
-      });
-    }
-
-    if (this._price < 0) {
-      this.notification.addError({
-        context: "product",
-        message: "Price must be greater than zero",
-      });
-    }
+    const validator = new ProductValidator();
+    validator.validate(this);
   }
 }
